ad8a5a397612380d64d346a0101dd9bfad62e1f2
Andrew Latham <lathama@gmail.com>
2017-05-03 11:38:23 -0500

    adding the demo code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # python-testing-example
 Example of Unittesting and Code Coverage being baked in
+https://github.com/lathama/python-testing-example
+
+Enabling your software to run its tests on every execution can be helpful
+to all parties. This can remove complexity with CI and local development stacks
+
+This is an example only meant to demonstrate the idea.
+
+* If Python Coverage is installed or the source is in "libraries" directory 
+  then code coverage will be run
+
+* Unit Testing will not exit on error but will log to unittesting.log
+
+* Actions driven by the module's __init__.py so look there.
+
---
+++ testingdemo/__init__.py
@@ -0,0 +1,42 @@
+"""
+ https://github.com/lathama/python-testing-example
+"""
+from __future__ import print_function
+import future_builtins
+import os, sys, time, unittest
+
+import_dir = "./libraries/"
+for d in os.listdir(import_dir):
+ if os.path.isdir(import_dir + d):
+  sys.path.insert(1, import_dir + d)
+
+__version__ = '1.0.0'
+TESTING = True
+
+from testingdemo.howto import AnExample
+
+if TESTING:
+  COVERAGE = False
+  try:
+    import coverage
+    COVERAGE = True
+    codecoverage = coverage.Coverage()
+    codecoverage.exclude("import", which="exclude")
+    codecoverage.start()
+  except:
+    COVERAGE = False
+    print("\n Coverage not importable, skipping code coverage testing")
+  unittesting_log = open('unittesting.log', 'w')
+  unittesting_log.write("Testing - " + str(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())) + "\n")
+  suite = unittest.TestLoader().discover('unittests')
+  unittest.TextTestRunner(stream=unittesting_log,descriptions=True,verbosity=3).run(suite)
+  unittesting_log.close()
+
+somethinguseful = AnExample()
+somethinguseful.make_something()
+somethinguseful.report_something()
+
+if COVERAGE:
+  codecoverage.stop()
+  codecoverage.save()
+  codecoverage.html_report(directory="coverage")
